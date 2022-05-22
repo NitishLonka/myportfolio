156eedec568e69964c5bb6c2c8589d1e94927ad2
Nitish Lonka <nitishlonka13@gmail.com>
2022-05-21 23:59:19 -0400

    Update skills.js

--- src/skills.js
+++ src/skills.js
@@ -1,10 +1,10 @@
 const skills=[
-    'Framework : ASP.NET Core',
-    'Languages : C, C++, C#, Java, React.js, HTML, Javascript, CSS',
+    'Framework : ASP.NET Core ,dotNet MVC',
+    'Languages : C, C++, C#, CoreJava, React.js, HTML, Javascript, CSS',
     'Databases : Oracle, MSSQL.',
-    'Tools : Visual Studio, Visual Studio Code, Postman, SQL Developer, SQL Server Management Studio, GitHub, Jira',
-    'Certifications: Azure Fundamentals(AZ-900, Microsoft Certification ID: 990767759)',
-    'Operating systems: Window'
+    'Tools : Visual Studio, Visual Studio Code, Postman, SQL Developer, SQL Server Management Studio, GitHub, Jira,Eclipse IDE',
+    'Certifications: Azure Fundamentals(AZ-900, Microsoft Certification ID: 990767759) ,MuleSoft Level-1 Developer',
+    'Operating systems: Windows'
 ]
 
 export default skills;
